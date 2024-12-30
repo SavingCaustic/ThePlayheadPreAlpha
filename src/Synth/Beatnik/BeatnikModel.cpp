7023adc9daef9703ac255ffbcae4bbb93fed77bd
Tjelvar Eriksson <savingcaustic@gmail.com>
2024-12-30 11:17:19 +0100

    beatnik cleanup

--- src/Synth/Beatnik/BeatnikModel.cpp
+++ src/Synth/Beatnik/BeatnikModel.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 namespace Synth::Beatnik {
-constexpr int VOICE_COUNT = 16;
+constexpr int VOICE_COUNT = 12;
 
 // Constructor to accept buffer and size
 Model::Model() {
@@ -30,104 +30,12 @@ void Model::setupParams(int upCount) {
     if (SynthBase::paramDefs.empty()) {
         // after declaration, indexation requested, see below..
         SynthBase::paramDefs = {
-            {UP::osc1_wf, {"osc1_wf", 0.0f, OSC1::WF::_count, false, 0, OSC1::WF::_count - 1, [this](float v) {
-                               std::cout << "setting osc1 wf to" << v << std::endl;
-                           }}},
-            {UP::osc1_fmsens, {"osc1_fmsens", 0.2f, 0, false, 0, 1, [this](float v) {
-                                   fmSens = v;
-                               }}},
-            /*{UP::osc1_amsens, {"osc1_amsens", 0.0f, 0, false, 0, 1, [this](float v) {
-                                   amSens = v;
-                               }}},*/
-            {UP::osc_mix, {"osc_mix", 0.5f, 0, false, 0, 1, [this](float v) {
-                               // easer should be at model. skipping easer for now
-                               oscMix = v;
-                           }}},
-            {UP::osc_detune, {"osc_detune", 0.5f, 0, false, -25, 25, [this](float v) {
-                                  // easer should be at model. skipping easer for now
-                                  oscDetune = v;
-                              }}},
-            {UP::osc1_senstrack, {"osc1_senstrack", 0.5f, 0, false, -1, 1, [this](float v) {
-                                      senseTracking = v;
-                                  }}},
-            {UP::osc2_semi, {"osc2_semi", 0.5f, 13, false, -6, 6, [this](float v) {
-                                 semitone = round(v);
-                             }}},
-            {UP::osc2_oct, {"osc2_oct", 0.5f, 7, false, -3, 3, [this](float v) {
-                                osc2octave = round(v);
-                            }}},
-            {UP::osc2_freqtrack, {"osc2_freqtrack", 1.0f, 0, false, 0, 1, [this](float v) {
-                                      osc2freqTrack = v;
-                                  }}},
-            {UP::vca_attack, {"vca_attack", 0.1f, 0, true, 2, 11, [this](float v) { // 8192 max
-                                  vcaAR.setTime(audio::envelope::ATTACK, v);
-                                  std::cout << "setting attack to " << v << " ms" << std::endl;
-                              }}},
-            {UP::vca_decay, {"vca_decay", 0.5f, 0, true, 5, 7, [this](float v) { // 8192 max
-                                 vcaAR.setTime(audio::envelope::DECAY, v);
-                                 std::cout << "setting decay to " << v << " ms" << std::endl;
-                             }}},
-            {UP::vca_sustain, {"vca_sustain", 0.8f, 0, false, 0, 1, [this](float v) {
-                                   vcaAR.setLevel(audio::envelope::SUSTAIN, v);
-                                   std::cout << "setting sustain to " << v << std::endl;
-                               }}},
-            {UP::vca_fade, {"vca_fade", 0.3f, 0, false, 0, 1, [this](float v) {
-                                vcaAR.setLeak(audio::envelope::FADE, v);
-                                std::cout << "setting fade to " << v << std::endl;
-                            }}},
-            {UP::vca_release, {"vca_release", 0.2f, 0, true, 10, 8, [this](float v) {
-                                   vcaAR.setTime(audio::envelope::RELEASE, v);
-                                   std::cout << "setting release to " << v << " ms" << std::endl;
-                               }}},
-            {UP::vca_spatial, {"vca_spatial", 0.2f, 0, false, 0, 1, [this](float v) {
-                                   vcaSpatial = v;
-                               }}},
-            {UP::vcf_type, {"vcf_type", 0.0f, 3, false, 0, 2, [this](float v) {
-                                // not totally safe but compact:
-                                filter.setFilterType(static_cast<audio::filter::FilterType>(static_cast<int>(v)));
-                                filter.initFilter();
-                            }}},
-            {UP::vcf_cutoff, {"vcf_cutoff", 0.5f, 0, true, 50, 8, [this](float v) {
-                                  filter.setCutoff(v);
-                                  filter.initFilter();
-                              }}},
-            {UP::vcf_resonance, {"vcf_resonance", 0.0f, 0, false, 0, 1, [this](float v) {
-                                     filter.setResonance(v);
-                                     filter.initFilter();
-                                 }}},
-            {UP::lfo1_shape, {"lfo1_shape", 0.0f, audio::lfo::LFOShape::_count, false, 0, audio::lfo::LFOShape::_count - 1, [this](float v) {
-                                  // of what really..
-                                  std::cout << "setting lfo1-shape to " << v << std::endl;
-                                  lfo1.setShape(static_cast<audio::lfo::LFOShape>(static_cast<int>(v)));
-                              }}},
-            {UP::lfo1_speed, {"lfo1_speed", 0.5f, 0, true, 100, 9, [this](float v) {
-                                  std::cout << "setting lfo1-speed (mHz) to " << v << std::endl;
-                                  lfo1.setSpeed(v); // in mHz.
-                              }}},
-            {UP::lfo1_routing, {"lfo1_routing", 0.3f, 6, false, 0, 5, [this](float v) {
-                                    lfo1Routing = static_cast<LFO1::Routing>(static_cast<int>(v));
-                                    std::cout << "setting lfo1-routing to " << v << std::endl;
-                                }}},
-            {UP::lfo1_depth, {"lfo1_depth", 0.0f, 0, false, 0, 1, [this](float v) {
-                                  // of what really..
-                                  std::cout << "setting lfo1-depth to " << v << std::endl;
-                                  lfo1depth = v;
-                              }}},
-            {UP::lfo2_shape, {"lfo2_shape", 0.0f, audio::lfo::LFOShape::_count, false, 0, audio::lfo::LFOShape::_count - 1, [this](float v) {
-                                  // of what really..
-                                  lfo2.setShape(static_cast<audio::lfo::LFOShape>(static_cast<int>(v)));
-                              }}},
-            {UP::lfo2_speed, {"lfo2_speed", 0.5f, 0, true, 100, 9, [this](float v) {
-                                  std::cout << "setting lfo1-speed (mHz) to " << v << std::endl;
-                                  lfo2.setSpeed(v); // in mHz.
-                              }}},
-            {UP::lfo2_routing, {"lfo2_routing", 0.0, LFO1::Routing::_count, false, 0, LFO1::Routing::_count - 1, [this](float v) {
-                                    lfo2Routing = static_cast<LFO2::Routing>(static_cast<int>(v));
-                                }}},
-            {UP::lfo2_depth, {"lfo2_depth", 0.0f, 0, false, 0, 1, [this](float v) {
-                                  // of what really..
-                                  lfo2depth = v;
-                              }}}};
+            {UP::a_pan, {"a_pan", 0.0f, 0, false, -1, 1, [this](float v) {
+                             // what would i need to do?
+                         }}},
+            {UP::b_pan, {"b_pan", 0.0f, 0, false, -1, 1, [this](float v) {
+                             // what would i need to do?
+                         }}}};
         // now reqeuest interface to reverse index.
         SynthBase::indexParams(upCount);
     }
@@ -163,8 +71,6 @@ void Model::parseMidi(uint8_t cmd, uint8_t param1, uint8_t param2) {
         SynthInterface::handleMidiCC(param1, fParam2);
         break;
     case 0xe0: {
-        int pitchBendValue = (static_cast<int>(param2) << 7) | static_cast<int>(param1);
-        bendCents = ((pitchBendValue - 8192) / 8192.0f) * 200.0f;
         break;
     }
     }
@@ -229,9 +135,6 @@ bool Model::renderNextBlock() {
         dist = dist * dist; // skip polarity..
         buffer[i] = synthBuffer[i] * (5 - dist) * 0.1f;
     }
-    // motherboard-stuff..
-    lfo1.updatePhase();
-    lfo2.updatePhase();
 
     // debugging
     if (false) {
@@ -244,20 +147,9 @@ bool Model::renderNextBlock() {
     return true;
 }
 
-float Model::getOscMix() {
-    return oscMix;
-}
-
 void Model::addToSample(std::size_t sampleIdx, float val) {
     // use local buffer for spead. Possibly double..
-    this->synthBuffer[sampleIdx] += val;
+    // this->synthBuffer[sampleIdx] += val;
 }
 
-const audio::osc::LUT &Model::getLUT1() const {
-    return lut1; // Return a const reference to lut1
-}
-
-const audio::osc::LUT &Model::getLUT2() const {
-    return lut2; // Return a const reference to lut2
-}
 } // namespace Synth::Beatnik
--- src/Synth/Beatnik/BeatnikModel.h
+++ src/Synth/Beatnik/BeatnikModel.h
@@ -5,7 +5,6 @@
 #include "core/audio/AudioMath.h"
 #include "core/audio/envelope/ADSFR.h"
 #include "core/audio/filter/MultiFilter.h"
-#include "core/audio/lfo/LFO.h"
 #include "core/audio/misc/Easer.h"
 #include "core/audio/osc/LUT.h"
 #include "core/parameters/params.h"
@@ -20,86 +19,14 @@ using json = nlohmann::json;
 namespace Synth::Beatnik {
 
 enum UP {
-    osc1_wf,
-    osc1_fmsens,
-    osc1_amsens,
-    osc1_senstrack,
-    osc2_semi,
-    osc2_oct,
-    osc2_freqtrack,
-    osc_mix,
-    osc_detune,
-    vca_attack,
-    vca_decay,
-    vca_sustain,
-    vca_fade,
-    vca_release,
-    vca_spatial,
-    vcf_type,
-    vcf_cutoff,
-    vcf_resonance,
-    lfo1_shape,
-    lfo1_speed,
-    lfo1_routing,
-    lfo1_depth,
-    lfo2_shape,
-    lfo2_speed,
-    lfo2_routing,
-    lfo2_depth,
+    a_pan,
+    b_pan,
+    c_pan,
+    d_pan,
     up_count
 };
 
-namespace LFO1 {
-enum Routing {
-    off,
-    osc1,
-    osc2,
-    osc12,
-    vcf,
-    vca,
-    _count
-};
-}
-
-namespace LFO2 {
-enum Routing {
-    off,
-    osc2,
-    vcf,
-    vca,
-    fmSens,
-    amSens,
-    _count
-};
-}
-
-namespace OSC1 {
-enum WF {
-    sine,
-    tri,
-    square,
-    saw,
-    noise,
-    lut1,
-    lut2,
-    _count
-};
-}
-
-namespace OSC2 {
-enum class WF {
-    sine,
-    tri,
-    square,
-    saw,
-    noise,
-    lut1,
-    lut2,
-    _count
-};
-}
-
-class Voice;
+class Voice; // forward declare
 class Model : public SynthBase {
 
   public:
@@ -124,54 +51,17 @@ class Model : public SynthBase {
     bool renderNextBlock() override;
 
     void addToSample(std::size_t sampleIdx, float val);
-    float getOscMix();
-
-    const audio::osc::LUT &getLUT1() const;
-    const audio::osc::LUT &getLUT2() const;
-
-    float oscMix = 0.5f;
-    float oscDetune = 0.0f;
-    float osc2freqTrack;
-    int semitone = 0;
-    int osc2octave = 0;
-    float bendCents = 0;
-    float senseTracking = 0.0f;
-    audio::envelope::ADSFR vcaAR;
-    float *buffer; // Pointer to audio buffer, minimize write so:
-    float synthBuffer[TPH_RACK_BUFFER_SIZE];
-    float fmSens = 0.0f;
-    LFO1::Routing lfo1Routing;
-    LFO2::Routing lfo2Routing;
-    audio::osc::LUT lut1;
-    audio::osc::LUT lut2;
-    std::size_t bufferSize; // Size of the audio buffer
-    float lfo1depth = 0.5;
-    float lfo2depth = 0.5;
-    audio::lfo::Standard lfo1; // change to ramp. duh. no, it's in voice..
-    audio::lfo::Standard lfo2;
-    audio::filter::MultiFilter filter;
-    float vcaSpatial;
-    float amSens;
 
   protected:
     std::vector<Voice> voices; // Vector to hold Voice objects
-
-    // float vcaEaser,
-    // vcaEaserStep;
-
-    // Parameter definitiongs
-    // std::unordered_map<std::string, ParamDefinition> parameterDefinitions;
-
-    // CC mapping
-    // std::unordered_map<int, std::string> ccMappings;
-
-    // Private methods
-    // void setupCCmapping(const std::string &path);
 
     // void renderVoice();
     int8_t findVoiceToAllocate(uint8_t note);
 
     void motherboardActions();
+
+    float *buffer; // Pointer to audio buffer, minimize write so:
+    float synthBuffer[TPH_RACK_BUFFER_SIZE];
 
     void initializeParameters();
     // Handle incoming MIDI CC messages
@@ -204,17 +94,16 @@ class Voice {
 
     void noteOn(uint8_t midiNote, float velocity) {
         notePlaying = midiNote;
-        // setup stuff based on note-value. But not frequency.
-        leftAtt = fmin(1, fmax(0, (notePlaying - 60) * 0.04f * modelRef.vcaSpatial));
-        rightAtt = fmin(1, fmax(0, (60 - notePlaying) * 0.04f * modelRef.vcaSpatial));
+        leftAtt = 1.0f;
+        rightAtt = 1.0f;
         noteVelocity = velocity;
-        tracking = fmax(0, (2.0f + modelRef.senseTracking * AudioMath::noteToFloat(notePlaying) * 7));
-        modelRef.vcaAR.triggerSlope(vcaARslope, audio::envelope::NOTE_ON);
+        // it's all in the voice. no AR in model.
+        vcaAR.triggerSlope(vcaARslope, audio::envelope::NOTE_ON);
     }
 
     void noteOff() {
         // enter release state in all envelopes.
-        modelRef.vcaAR.triggerSlope(vcaARslope, audio::envelope::NOTE_OFF);
+        vcaAR.triggerSlope(vcaARslope, audio::envelope::NOTE_OFF);
     }
 
     audio::envelope::ADSFRState getVCAstate() {
@@ -230,51 +119,18 @@ class Voice {
     }
 
     bool renderNextVoiceBlock(std::size_t bufferSize) {
-        float osc1hz, osc2hz;
-        // Chunk could be set to SIMD-capability.
         constexpr int chunkSize = 16;
         if (vcaARslope.state != audio::envelope::OFF) {
-            // put stuff here that should be interactive on note on.
-            float fmAmp = tracking * modelRef.fmSens * noteVelocity;
-            if (modelRef.lfo2Routing == LFO2::Routing::fmSens) {
-                fmAmp *= (1 + modelRef.lfo2.getLFOval() * modelRef.lfo2depth);
-            }
-            AudioMath::easeLog50(fmAmp, fmAmpEaseOut);
-            float mixAmplitude = noteVelocity * 0.6f;
-            AudioMath::easeLog5(modelRef.oscMix, oscMixEaseOut);
-            int osc2note = notePlaying + modelRef.semitone + modelRef.osc2octave * 12;
-            float osc2cents = modelRef.bendCents + modelRef.oscDetune;
-            if (modelRef.lfo1Routing == LFO1::Routing::osc12 || modelRef.lfo1Routing == LFO1::Routing::osc2) {
-                osc2cents += modelRef.lfo1.getLFOval() * modelRef.lfo1depth * 1200.0f;
-            }
-            osc2hz = AudioMath::noteToHz(osc2note, osc2cents);
-            osc2.setAngle(osc2hz);
-            osc1hz = AudioMath::noteToHz(notePlaying, modelRef.bendCents - modelRef.oscDetune);
-            osc1.setAngle(osc1hz);
-            // setup new delta (lin-easer) for VCA
-            modelRef.vcaAR.updateDelta(vcaARslope);
-            float vcaTarget = vcaARslope.currVal + vcaARslope.gap;
-            if (modelRef.lfo1Routing == LFO1::Routing::vca) {
-                vcaTarget *= (modelRef.lfo1.getLFOval() * modelRef.lfo1depth) + 1.0f;
-            }
-            if (modelRef.lfo2Routing == LFO2::Routing::vca) {
-                vcaTarget *= (modelRef.lfo2.getLFOval() * modelRef.lfo2depth) + 1.0f;
-            }
-            vcaEaser.setTarget(vcaTarget);
-
             for (std::size_t i = 0; i < bufferSize; i += chunkSize) {
                 for (std::size_t j = 0; j < chunkSize; j = j + 2) {
                     float y2 = osc2.getNextSample(0);
-                    vcaEaserVal = vcaEaser.getValue();
-                    float y1 = osc1.getNextSample(y2 * fmAmp * (vcaEaserVal + 0.3f)) * (1.0f + modelRef.amSens * y2);
-                    // now weight sample to channel based on noteval.
-                    float voiceOut = ((y1 * (1 - oscMixEaseOut) + y2 * oscMixEaseOut)) * vcaEaserVal * mixAmplitude;
+                    float voiceOut = y2;
                     modelRef.addToSample(i + j, voiceOut * (1 - leftAtt));
                     modelRef.addToSample(i + j + 1, voiceOut * (1 - rightAtt));
                 }
             }
             // copy local voice-buffer to synth-buffer
-            modelRef.vcaAR.commit(vcaARslope);
+            vcaAR.commit(vcaARslope);
         } else {
             // Remove voice from playing
             // maybe use return-type
@@ -287,6 +143,7 @@ class Voice {
   protected:
     audio::osc::LUTosc osc1;
     audio::osc::LUTosc osc2;
+    audio::envelope::ADSFR vcaAR;
     audio::envelope::ADSFRSlope vcaARslope;
     audio::misc::Easer oscMixEaser;
     audio::misc::Easer vcaEaser;
